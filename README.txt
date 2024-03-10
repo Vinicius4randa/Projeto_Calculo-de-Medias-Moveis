88dd46c3df520e1ec6790f110128f437b74315c1
VAranda <124964947+v-aranda@users.noreply.github.com>
2024-03-10 01:53:31 -0300

    Media movel e todolist

--- README.txt
+++
@@ -1,3 +0,0 @@
-REFERÊNCIAS:
-
-Seções de 3.10 a 3.17: https://panda.ime.usp.br/pythonds/static/pythonds_pt/03-EDBasicos/toctree.html
--- deque.py
+++ deque.py
@@ -1,9 +1,11 @@
-class FilaArray:
-    INIT_CAP = 3
-    def __init__(self):
-        self._dados = [None] * FilaArray.INIT_CAP
+class Data_Intercection:
+    def __init__(self,Maxlen=3):
+        self._dados = [None] * Maxlen
         self._tamanho = 0
         self._inicio = 0
+        
+        self.janela = Maxlen
+
     def __len__(self):
         return self._tamanho
     def is_empty(self):
@@ -14,7 +16,24 @@ class FilaArray:
     def first(self):
         return self._dados[self._inicio]
     
-    def dequeue(self):
+    def next(self, e):
+        if self._tamanho == len(self._dados):
+            self._dequeue()
+            self._collection_step()
+        disponivel = (self._inicio + self._tamanho) %len(self._dados)
+        self._dados[disponivel] = e
+        self._tamanho += 1
+    def media_movel(self):
+        soma = 0
+        for i in self._dados:
+            if not i:
+                return None
+            soma += i
+            
+        return round(soma/self.janela)
+
+    
+    def _dequeue(self):
         if self.is_empty():
             raise FilaVazia('A Fila está vazia')
         result = self._dados[self._inicio]
@@ -22,19 +41,23 @@ class FilaArray:
         self._inicio = (self._inicio + 1) %len(self._dados)
         self._tamanho -= 1
         return result
-    def enqueue(self, e):
-        if self._tamanho == len(self._dados):
-            self._aumenta_tamanho(2 * len(self._dados))
-        disponivel = (self._inicio + self._tamanho) %len(self._dados)
-        self._dados[disponivel] = e
-        self._tamanho += 1
-    def _aumenta_tamanho(self, novo_tamanho):
+    
+    def _collection_step(self):
         dados_antigos = self._dados 
-        self._dados = [None] * novo_tamanho 
+        self._dados = [None] * self.janela
         posicao = self._inicio
         for k in range(self._tamanho): 
             self._dados[k] = dados_antigos[posicao]
             posicao = (1 + posicao) % len(dados_antigos)
         self._inicio = 0
 
-teste = FilaArray()
+teste = Data_Intercection()
+dados = [10,15,12,5,2,3,1]
+saida = []
+
+for i in dados:
+    teste.next(i)
+    saida.append(teste.media_movel())
+
+print(saida) 
+
